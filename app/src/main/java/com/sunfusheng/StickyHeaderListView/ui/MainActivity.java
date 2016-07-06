778deb4463b6f2f9128934903e5645a6f6f1f3e5
a4611764a <954884146@qq.com>
2016-07-06 17:41:13 +0800

    8888

--- app/src/main/java/com/sunfusheng/StickyHeaderListView/ui/MainActivity.java
+++ app/src/main/java/com/sunfusheng/StickyHeaderListView/ui/MainActivity.java
@@ -40,7 +40,7 @@ import butterknife.ButterKnife;
 
 /**
  * 作者fexzzzdddt
- *666
+ *8888
  * 个人博客：sunfusheng.com
  */
 public class MainActivity extends AppCompatActivity implements SmoothListView.ISmoothListViewListener {
